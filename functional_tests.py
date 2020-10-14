065a01c90ce39984954eba644eafc5226a36ec4f
Alechan <mail@domain.com>
2020-10-14 20:17:02 -0300

    microsimu-32: .idea files

--- functional_tests.py
+++ functional_tests.py
@@ -3,7 +3,7 @@ import requests
 server_url = "http://localhost:8000"
 
 
-def test_get_locations_for_us_90210_check_status_code_equals_200():
+def test_main_url_returns_200_with_get():
     response = requests.get(server_url)
     assert response.status_code == 200
 
