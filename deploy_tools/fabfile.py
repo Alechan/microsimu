deafa94e6ef2328a390591df94ca904dab207385
Alechan <mail@domain.com>
2020-11-01 18:17:28 -0300

    #50: add fabfile for deploys

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,38 @@
+from fabric.api import cd, env, local, run
+from fabric.contrib.files import exists
+
+
+def deploy(git_repo, branch, server_username):
+    site_folder = f'/home/{server_username}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source(git_repo, branch)
+        _docker_compose_down()
+        _docker_compose_up()
+        _update_static_files()
+        _update_database()
+
+
+def _get_latest_source(git_repo, branch):
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run(f'git clone {git_repo} .')
+    run(f'git checkout {branch}')
+    run(f'git pull origin {branch}')
+
+
+def _docker_compose_down():
+    run('docker-compose down')
+
+
+def _docker_compose_up():
+    run('docker-compose --build -d')
+
+
+def _update_static_files():
+    run('docker-compose exec -T web python manage.py migrate --noinput')
+
+
+def _update_database():
+    run('docker-compose exec -T web python manage.py collectstatic --noinput')
