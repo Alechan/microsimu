8abf06852254ae16a809adc1ab38ca3040b9b831
Alechan <mail@domain.com>
2020-11-07 21:37:05 -0300

    #78: add fortran lawm simulator

---
+++ src/api/std_lib/tests/lawm/test_fortran_simulator.py
@@ -0,0 +1,9 @@
+from unittest import skip
+
+from django.test import TestCase
+
+
+class LAWMFortranSimulatorTest(TestCase):
+    @skip("Simulate endpoint POST request not finished yet")
+    def test_given_the_run_parameters_(self):
+        self.fail("")
