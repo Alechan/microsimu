1536f02a351ef6242b4097aee5722a5c3ccfe9a6
Alechan <mail@domain.com>
2020-11-08 23:48:27 -0300

    #78: refactor lawm standard run into lawm's own folder

--- src/api/tests/helpers/api_test_mixin.py
+++ src/api/tests/helpers/api_test_mixin.py
@@ -1,5 +1,6 @@
 import pandas
 
+from api.api_settings import LAWM_CSV_PER_REGION
 from api.std_lib.lawm.regions import Africa, Latinamerica, Developed, Asia
 from api.tests.helpers.database_tree import TestDatabaseTree
 from api.tests.helpers.django_asserts_mixin import DjangoAssertsMixin
@@ -10,11 +11,11 @@ from microsimu.settings import BASE_DIR
 
 class MicroSimuTestMixin(GeneralAssertsMixin, DjangoAssertsMixin, LAWMAssertsMixin):
     BASE_SERVER_URL     = 'http://testserver'
-    MIGRATIONS_CSV_PATH = BASE_DIR / "api" / "migrations" / "csvs"
-    STD_RUN_DEVELOPED_CSV_PATH  = MIGRATIONS_CSV_PATH / "fortran_std_developed.csv"
-    STD_RUN_LA_CSV_PATH         = MIGRATIONS_CSV_PATH / "fortran_std_la.csv"
-    STD_RUN_AFRICA_CSV_PATH     = MIGRATIONS_CSV_PATH / "fortran_std_africa.csv"
-    STD_RUN_ASIA_CSV_PATH       = MIGRATIONS_CSV_PATH / "fortran_std_asia.csv"
+
+    STD_RUN_DEVELOPED_CSV_PATH = LAWM_CSV_PER_REGION[Developed.name]
+    STD_RUN_LA_CSV_PATH        = LAWM_CSV_PER_REGION[Latinamerica.name]
+    STD_RUN_AFRICA_CSV_PATH    = LAWM_CSV_PER_REGION[Africa.name]
+    STD_RUN_ASIA_CSV_PATH      = LAWM_CSV_PER_REGION[Asia.name]
     STD_RUN_REGIONS_DFS_TUPLES = [
         [Developed.name, pandas.read_csv(STD_RUN_DEVELOPED_CSV_PATH)],
         [Latinamerica.name, pandas.read_csv(STD_RUN_LA_CSV_PATH)],
