8395e5b67ef663fc75cbf546d4ae5494a29e6baa
Alechan <mail@domain.com>
2020-11-14 18:40:29 -0300

    #112: make link to "/visualize" dynamically generated in region result

--- src/api/views.py
+++ src/api/views.py
@@ -216,9 +216,7 @@ class RegionResultDetail(APIView):
     Get a region's (Developed Countries, Latin-America, Africa, Asia) results for
     a simulation.
 
-    Append "/visualize" to this endpoint to get an HTML
-    file with visualization examples using the data show
-    below. 
+    You can find visualization examples for these results here: <{visualize_url}>
     """
     renderer_classes = APIView.renderer_classes
 
@@ -231,6 +229,22 @@ class RegionResultDetail(APIView):
         region_result = get_object_or_404(simu.region_results, region__name=region_name)
         serializer = RegionResultSerializer(region_result, context={"request": request})
         return Response(serializer.data)
+
+    @property
+    def description(self):
+        """
+        Replace the variables in the description with the correct url. This way, the url of
+        where the microservice is being accessed from doesn't need to be hardcoded and instead
+        depends on the request.
+
+        :return: A SafeString (probably) with the description in HTML
+        """
+        base_description = self.__class__.__doc__
+        visualize_kwargs = self.request.parser_context["kwargs"]
+        description = base_description.format(
+            visualize_url=self.request.build_absolute_uri(reverse("api:visualize", kwargs=visualize_kwargs)),
+        )
+        return description
 
 
 class VisualizeView(View):
